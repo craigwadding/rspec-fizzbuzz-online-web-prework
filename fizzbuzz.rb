e1c7ae6bbc820c34a9719d278a1aedd04832731d
craig wadding <craig.wadding@gmail.com>
2019-03-10 22:26:27 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,13 +3,13 @@
 def fizzbuzz(int)
   if int % 15 == 0 
     puts "FizzBuzz"
-  end
+  
   elsif int % 5 == 0 
     puts "Fizz"
-  end
+  
   elsif int % 3 == 0 
     puts "Buzz"
-  end
+  
   else 
     puts "nil"
   end
